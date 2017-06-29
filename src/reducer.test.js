25db323d192d28d870be78d0bfdc462c822e7330
tckastanek <tckastanek@gmail.com>
2017-06-29 12:28:25 -0400

    Reducer tests complete.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,7 +1,13 @@
 import reducer from './reducer';
 import * as actions from './actions';
 
-describe.only('reducer', () => {
+describe('reducer', () => {
+  const guess1 = 100;
+  const guess2 = 80;
+  const guess3 = 40;
+  const guess4 = 49;
+  const guess5 = 50;
+  const invalidGuess = 'twenty';
 
   it('Should show the initialState state where nothing is passed in', () => {
     const state = reducer(undefined, {type: '__Unknown'});
@@ -16,16 +22,69 @@ describe.only('reducer', () => {
     const state = reducer(currentState, {type: '_Unknown'});
     expect(state).toBe(currentState);
   });
-});
 
-describe('newGame', () => {
 
-});
+  describe('newGame', () => {
+    it('Should reset the game', () => {
+      let state;
+      state = reducer(state, actions.newGame());
+      expect(state.guesses).toEqual([]);
+      expect(state.feedback).toEqual('Make your guess!');
+      expect(state.correctAnswer).toBeGreaterThanOrEqual(1);
+      expect(state.correctAnswer).toBeLessThanOrEqual(100);
+      expect(state.showInfoModal).toEqual(false);
+    });
+  });
 
-describe('makeGuess', () => {
+  describe('makeGuess', () => {
+    it('Should add a guess to the guesses array', () => {
+      let state;
+      state = reducer(state, actions.makeGuess(guess1));
+      state = reducer(state, actions.makeGuess(guess2));
+      state = reducer(state, actions.makeGuess(guess3));
+      state = reducer(state, actions.makeGuess(guess4));
+      state = reducer(state, actions.makeGuess(guess5));
+      state = reducer(state, actions.makeGuess(invalidGuess));
+      expect(state.guesses).toEqual([guess1, guess2, guess3, guess4, guess5]);
+    });
+    it('Should provide the right feedback', () => {
+      let state = {
+        guesses: [],
+        feedback: 'Make your guess!',
+        correctAnswer: guess5,
+        showInfoModal: false
+      };
+      state = reducer(state, actions.makeGuess(guess1));
+      expect(state.feedback).toEqual('You\'re Ice Cold...');
+      state = reducer(state, actions.makeGuess(guess2));
+      expect(state.feedback).toEqual('You\'re Cold...');
+      state = reducer(state, actions.makeGuess(guess3));
+      expect(state.feedback).toEqual('You\'re Warm');
+      state = reducer(state, actions.makeGuess(guess4));
+      expect(state.feedback).toEqual('You\'re Hot!');
+      state = reducer(state, actions.makeGuess(guess5));
+      expect(state.feedback).toEqual('You got it!');
+      state = reducer(state, actions.makeGuess(invalidGuess));
+      expect(state.feedback).toEqual('Please enter a valid number');
+    });
 
-});
+  });
 
-describe('toggleInfoModal', () => {
-
+  describe('toggleInfoModal', () => {
+    it('Should toggle the info modal', () => {
+      let state;
+      state = reducer(state, actions.toggleInfoModal());
+      expect(state.guesses).toEqual([]);
+      expect(state.feedback).toEqual('Make your guess!');
+      expect(state.correctAnswer).toBeGreaterThanOrEqual(1);
+      expect(state.correctAnswer).toBeLessThanOrEqual(100);
+      expect(state.showInfoModal).toEqual(true);
+      state = reducer(state, actions.toggleInfoModal());
+      expect(state.guesses).toEqual([]);
+      expect(state.feedback).toEqual('Make your guess!');
+      expect(state.correctAnswer).toBeGreaterThanOrEqual(1);
+      expect(state.correctAnswer).toBeLessThanOrEqual(100);
+      expect(state.showInfoModal).toEqual(false);
+    });
+  });
 });
