32560fd096ba86619c0dcdf50eb9a205b2c7d037
patricewhite <patricewhite2012@gmail.com>
2017-06-29 12:45:17 -0400

    2/3 action test passing

---
+++ src/actions.test.js
@@ -0,0 +1,23 @@
+import * as actions from './actions';
+
+describe('newGame', () => {
+  it('should return the action', () => {
+    const action = actions.newGame();
+    expect(action.type).toEqual(actions.NEW_GAME);
+    expect(action.correctAnswer).toBeGreaterThanOrEqual(1);
+    expect(action.correctAnswer).toBeLessThanOrEqual(100);
+  });
+});
+
+describe('makeGuess', () =>{
+  it('should return the action', () => {
+    const guess = 6;
+    const action = actions.makeGuess(guess);
+    expect(action.type).toEqual(actions.MAKE_GUESS);
+    expect(action.guess).toEqual(guess);
+  });
+});
+
+describe('toggleInfoModal', () => {
+
+});
